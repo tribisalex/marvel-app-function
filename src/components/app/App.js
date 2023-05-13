f639d58f994c76b7de9b612b536584b8fdd88611
Alexandr <tribisalexandr@yandex.ru>
2023-05-13 18:09:44 +0300

    feat: remove SinglePage component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import AppHeader from "../appHeader/AppHeader";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {ComicsPage, MainPage} from "../../pages";
+import SingleComic from "../../pages/SingleComicPage";
 
 const App = () => {
 
@@ -12,6 +13,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<MainPage/>} />
             <Route path="/comics" element={<ComicsPage/>} />
+            <Route path="/comics/comicsId" element={<SingleComic />} />
           </Routes>
         </main>
       </div>
